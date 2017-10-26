bab56272e35b6dc420297dec7b6d2dc9b8daad1b
minjie <minjie@shanghaitech.edu.cn>
2017-10-26 22:11:42 +0800

    enhance

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ So if you want to watch the video ---> first run /src/video/img2video.py then ru
 So if you want to recreate my result ---> run /src/video/main.py  and run /src/photos/main.py
 
 Then create the video ---> run /src/video/img2video.py 
+
+By the way after I RUN several frames cost a lot of time, later I come up with a new fliter idea to enhance my photos, So just run enhance.py in the folder
--- src/photos/enhance.py
+++ src/photos/enhance.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
+import os
 for i in range(1,4):
-    image = cv2.imread(str(i)+".jpg")
+    image = cv2.imread(os.getcwd()+"\\..\\..\\photos\\"+str(i)+".jpg")
     image = cv2.GaussianBlur(image, (3, 3), 0.4)
     M = np.ones(image.shape,dtype="uint8")*32
     added = cv2.add(image,M)
-    cv2.imwrite(str(i)+".jpg",added)
+    cv2.imwrite(os.getcwd()+"\\..\\..\\photos\\"+str(i)+".jpg",added)
 
